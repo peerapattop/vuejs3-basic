eeb851f95e73e5b9d3df1173bea9819f94b67d05
peerapattop <peerapatza401@gmail.com>
2023-05-31 00:26:22 +0700

    ฟังชั่น Data, Interpolation, อ่านค่าแท็ก HTML

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,13 @@
 <template>
   <section>
-    <h1>peera</h1>
-    <h2>rov</h2>
-    <h1>tangpanyawai</h1>
+    <!-- Interpolation ด้วย Data-->  
+    <!-- {{firstname}} -->
+
+    <h1>ชื่อ : {{firstname}}</h1>  
+    <h1>นามสกุล : {{lastname}}</h1>
+    <h1>อายุ : {{age}} ปี</h1>
+    <p>ที่อยู่ : <span v-html="address"></span></p> 
+    <!-- ใช้ "v-html" เพื่ออ่านค่าแท็ก HTML-->
   </section>
 </template>
 
@@ -11,6 +16,16 @@
 
 export default {
   name: 'App',
+  // ฟังชั่นก์ data จะส่งค่ากลับมาในรูปแบบ Object โดยค่าหรือข้อมูลที่ส่งกลับมานั่นสามารถนำไปใช้ใน template หรือ ส่วนอื่นๆของ Component
+  data(){
+    return{
+      firstname:"พีรพัฒน์",
+      lastname:"ตั้งปัญญาไว",
+      age:21,
+      address:"<strong>กรุงเทพมหานคร</strong>"
+    }
+
+  }
 
 }
 </script>
